79c3bc2b795e1244d95ea5dcb7a2b05a4264514d
Zachary R Douglas <Zachrdouglas@outlook.com>
2026-02-12 11:13:50 -0500

    Add chemicals array with water details

---
+++ script.js
@@ -0,0 +1,12 @@
+const chemicals = [
+  {
+    name: "water"
+    formula: "H₂O"
+    facts: [
+      "Water is literally everywhere and we drink it everyday.",
+      "It doesn't mix with oil no matter how hard you shake it.",
+      "Ice floats because it's actually less dense than liquid water.",
+      "Water can absorb a lot of heat before getting hot.",
+      ]
+},
+    
